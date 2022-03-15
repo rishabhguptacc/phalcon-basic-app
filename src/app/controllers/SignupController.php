57b0e7128d30bc1429080adc7d9edd7f990b73d6
Rishabh Gupta <rishabhgupta@cedcoss.com>
2022-03-15 15:48:02 +0530

    Initially provided files.

---
+++ src/app/controllers/SignupController.php
@@ -0,0 +1,38 @@
+<?php
+
+use Phalcon\Mvc\Controller;
+
+class SignupController extends Controller{
+
+    public function IndexAction(){
+
+    }
+
+    public function registerAction(){
+        $user = new Users();
+
+        $user->assign(
+            $this->request->getPost(),
+            [
+                'name',
+                'email'
+            ]
+        );
+
+        $success = $user->save();
+
+        $this->view->success = $success;
+
+        if($success){
+            $this->view->message = "Register succesfully";
+        }else{
+            $this->view->message = "Not Register succesfully due to following reason: <br>".implode("<br>", $user->getMessages());
+        }
+    }
+
+    // Here I'm trying to do something... textAction()
+
+    public function textAction(){
+        echo "test on the textAction() in signupController.php in controller...";
+    }
+}
